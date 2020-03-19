2bed4277bab4a974594dca3abac336d49a89fe82
Stefan Brugger <stefpb@gmail.com>
2020-03-19 13:03:36 +0000

    change F to use Linear

--- model.py
+++ model.py
@@ -392,7 +392,7 @@ class pc_conv_network(nn.Module):
 		### Precision ### 
 		dimension = self.chan[i+1] * self.imdim[i+1]^2
 		self.Precision = ModuleList(
-			[Bilinear(dimension, dimension, 1, bias=False)
+			[nn.Bilinear(dimension, dimension, 1, bias=False)
 			for i in range(-1,self.nlayers)])
 
 		#self.Sigma = nn.ParameterList([nn.Parameter(torch.diag(torch.ones(self.chan[i+1] * self.imdim[i+1] * self.imdim[i+1])))
