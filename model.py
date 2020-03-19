fea8dbb983bf3e27c75b24319cb891f47ded615c
Stefan Brugger <stefpb@gmail.com>
2020-03-19 13:55:19 +0000

    change F to use Linear

--- model.py
+++ model.py
@@ -415,7 +415,7 @@ class pc_conv_network(nn.Module):
 		if i > 0:
 			PE_0 = self.phi[i-1] - (self.conv_trans[i](F.relu(self.phi[i].view(self.bs, self.chan[i+1], self.imdim[i+1], self.imdim[i+1])))).view(self.bs,-1)
 		else:
-			PE_0 = self.images   - (self.conv_trans[i](F.relu(self.phi[i].view(self.bs, self.chan[i+1], self.imdim[i+1], self.imdim[i+1])))).view(self.bs,-1)
+			PE_0 = self.images   - (self.conv_trans[i](F.relu(self.phi[i].view(self.bs, self.chan[i+1], self.imdim[i], self.imdim[i])))).view(self.bs,-1)
 
 		if i == self.nlayers-1:
 			PE_1 = self.phi[i] - self.top_cause
