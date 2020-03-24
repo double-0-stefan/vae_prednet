F with bilinear for precision - more fixes

model.py
@@ -378,15 +378,17 @@ class pc_conv_network(nn.Module):
 	def init_conv_trans(self, p):
 
 		self.conv_trans = ModuleList(
-			[ConvTranspose2d(p['chan'][i+1], p['chan'][i], p['ks'][i], 1,p['pad'][i]).cuda()
+			[ConvTranspose2d(p['chan'][i+1], p['chan'][i], p['ks'][i], 1,p['pad'][i])
 			for i in range(self.nlayers)])
+		self.conv_trans.cuda()
 	#	for i in range(self.layers):
 	#		self.conv_trans[i].weight = torch.nn.Parameter(torch.eye())
 
 	def init_phi(self,p):
 		conv = ModuleList(
 			[Conv2d(p['chan'][i], p['chan'][i+1], p['ks'][i], 1,p['pad'][i])
-			for i in range(self.nlayers)]).cuda()
+			for i in range(self.nlayers)])
+		conv.cuda()
 		x = torch.zeros(self.bs,1,32,32).cuda()
 		#phi = [nn.Parameter(torch.rand(self.bs,1*32*32))] # mnist
 		phi = []