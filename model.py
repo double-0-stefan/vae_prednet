F with bilinear for precision - more fixes

model.py
@@ -412,7 +412,7 @@ class pc_conv_network(nn.Module):
 			for i in range(self.nlayers+1)]).cuda()
 
 		for i in range(self.nlayers+1):
-			weights = torch.eye(self.chan[i]*self.imdim[i]*self.imdim[i]).unsqueeze(0)
+			weights = torch.exp(8) * torch.eye(self.chan[i]*self.imdim[i]*self.imdim[i]).unsqueeze(0)
 			self.Precision[i].weight = nn.Parameter(weights.cuda())
 
 	def reset(self):
@@ -492,6 +492,7 @@ class pc_conv_network(nn.Module):
 			self.loss(l)
 		self.F.backward()
 		self.optimizer.step()
+		print(self.Precision[0].weight)
 
 
 	def forward(self, iteration, images, learn=1):