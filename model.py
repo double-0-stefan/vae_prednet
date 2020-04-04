db689784046ca338b56766ca3100d8a7b80d4141
Stefan Brugger <stefpb@gmail.com>
2020-04-04 15:45:04 +0100

    partial precisions

--- model.py
+++ model.py
@@ -59,7 +59,7 @@ class pc_conv_network(nn.Module):
 
 	def init_conv_trans(self, p): # does conv, phi and precision
 		if p['imdim']:
-			x = torch.zeros(self.bs,1,p['imdim'],p['imdim'])
+			x = torch.zeros([self.bs,1,p['imdim'],p['imdim']])
 		else:
 			x = torch.zeros(self.bs,1,32,32)
 		p['imdim'] = [x.size(2)]
