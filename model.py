2af2ca66754e4b7fa68ba5a36d663d7d649dac15
Stefan Brugger <stefpb@gmail.com>
2020-04-04 21:57:55 +0100

    partial precisions

--- model.py
+++ model.py
@@ -63,7 +63,8 @@ class pc_conv_network(nn.Module):
 		# else:
 		x = torch.zeros(self.bs,1,33,33)
 		#	p['imdim'] = [x.size(2)]
-		imdim = p['imdim']
+		imdim = []
+		imdim.append(p['imdim'])
 		self.conv_trans = []
 		conv = []
 		phi = []
