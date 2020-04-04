2a3e8d313f0c8e20324323bf81f604d52349d760
Stefan Brugger <stefpb@gmail.com>
2020-04-04 17:12:38 +0100

    partial precisions

--- model.py
+++ model.py
@@ -88,11 +88,12 @@ class pc_conv_network(nn.Module):
 				
 				conv_block.append(Conv2d(p['chan'][j][i], p['chan'][j][i+1], p['ks'][j][i], 1,p['pad']))
 			
-			block.append(ConvTranspose2d(p['chan'][j+1][0], p['chan'][j][-1], p['ks'][j][-1], 1,
-				p['pad']))
+			# if j < p['nblocks'] - 1:
+			# 	block.append(ConvTranspose2d(p['chan'][j+1][0], p['chan'][j][-1], p['ks'][j][-1], 1,
+			# 		p['pad']))
 			
-			conv_block.append(Conv2d(p['chan'][j][-1], p['chan'][j+1][0], p['ks'][j][-1], 1,
-				p['pad']))
+			# 	conv_block.append(Conv2d(p['chan'][j][-1], p['chan'][j+1][0], p['ks'][j][-1], 1,
+			# 		p['pad']))
 
 			self.conv_trans.append(block)
 			conv.append(conv_block)
