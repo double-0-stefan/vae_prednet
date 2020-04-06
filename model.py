b455e1c34e41e2920b3bb4e733165e244bc2d679
Stefan Brugger <stefpb@gmail.com>
2020-04-06 13:56:03 +0100

    partial precision

--- model.py
+++ model.py
@@ -366,7 +366,7 @@ class pc_conv_network(nn.Module):
 
 	def forward(self, iteration, images, learn=1):
 
-		print(self)
+		#print(self)
 		self.optimizer = Adam(self.parameters(), lr=self.p['lr'], weight_decay=1e-5)
 		self.iteration = iteration
 		self.F_last = self.F
