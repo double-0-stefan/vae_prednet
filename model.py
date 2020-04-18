e6e0b5f87c60508601b5851e3e707f0439e52a5e
Stefan Brugger <stefpb@gmail.com>
2020-04-18 18:31:46 +0100

    add latents

--- model.py
+++ model.py
@@ -528,7 +528,8 @@ class pc_conv_network(nn.Module):
 				self.loss(l,learn=0)
 
 
-
+			print(self.kl_loss)
+			print(self.F)
 			self.F += torch.sum(torch.tensor(self.kl_loss))
 			# if i < self.iter-1:
 			# 	self.F.backward(retain_graph=True)
