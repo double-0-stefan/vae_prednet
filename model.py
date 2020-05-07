6491469b452201750b19ab27827da788e68d1a5c
Stefan Brugger <stefpb@gmail.com>
2020-05-07 20:56:34 +0100

    get print working

--- model.py
+++ model.py
@@ -777,7 +777,7 @@ class pc_conv_network(nn.Module):
 		
 		# random latents
 		#self.z_pc = nn.Parameter(torch.rand(self.bs,self.latents))
-		# print(images)
+		print(images[0,0,:,:])
 		self.iteration = iteration
 		# del self.F
 
