faab5582bc380c1be31924ff257a04854e42625b
Stefan Brugger <stefpb@gmail.com>
2020-05-21 15:42:21 +0100

    conv covariance

--- model.py
+++ model.py
@@ -278,8 +278,8 @@ class pc_conv_network(nn.Module):
 
 			vv = v[:,:,j]
 			other_weights = vv[fi]
-			print(other_weights.numel())
-			b[j, j+1:j+1+1+other_weights.numel()] = other_weights
+			# print(other_weights.numel())
+			b[j, j+1:j+1+other_weights.numel()] = other_weights
 
 		# paste this into matrix, length(centres) number of times,
 		# add zeros to make square
