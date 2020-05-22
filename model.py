cbc27f8c54d56f73e8a078eb08d5ec43cd5e3464
Stefan Brugger <stefpb@gmail.com>
2020-05-22 15:28:40 +0100

    conv covariance

--- model.py
+++ model.py
@@ -505,15 +505,20 @@ class pc_conv_network(nn.Module):
 		
 		elif self.p['conv_precision']:
 
+			if i > 0:
+				chan = self.chan[i-1][-1]
+			else:
+				chan = self.p['imchan']
+
 			if learn == 1:
 				f = 0.5*sum(sum(
 					- self.logdet_block_tridiagonal(i) # -ve here because more precise = good (nb will need to balance over layers somehow)
-					+ torch.mm(PE, (self.Precision[i](PE.view(self.phi[i].size())) ).view(-1).t())
+					+ torch.mm(PE, (self.Precision[i](PE.view(self.bs, chan, self.dim[i][-1], self.dim[i][-1]))).view(self.p['bs'],-1).t())
 					))
 			else:
-				print(self.chan)
+
 				f = 0.5*sum(sum(
-					torch.mm(PE, (self.Precision[i](PE.view(self.bs, self.chan[i][-1], self.dim[i][-1], self.dim[i][-1]))).view(self.p['bs'],-1).t())
+					torch.mm(PE, (self.Precision[i](PE.view(self.bs, chan, self.dim[i][-1], self.dim[i][-1]))).view(self.p['bs'],-1).t())
 					))
 			print(f)
 
