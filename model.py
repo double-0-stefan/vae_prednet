5d4cc39f6dac97ef9175b24dae48c8e1d9dacdd7
Stefan Brugger <stefpb@gmail.com>
2020-05-22 14:49:49 +0100

    conv covariance

--- model.py
+++ model.py
@@ -511,6 +511,7 @@ class pc_conv_network(nn.Module):
 					+ torch.mm(PE, (self.Precision[i](PE.view(self.phi[i].size())) ).view(-1).t())
 					))
 			else:
+				print(self.chan)
 				f = 0.5*sum(sum(
 					torch.mm(PE, (self.Precision[i](PE.view(self.bs, self.chan[i][-1], self.dim[i][-1], self.dim[i][-1]))).view(self.p['bs'],-1).t())
 					))
