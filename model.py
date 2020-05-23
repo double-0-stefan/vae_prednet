f0af4575acf214ea80c7dbb1b01560cb25a3e0c6
Stefan Brugger <stefpb@gmail.com>
2020-05-23 19:37:33 +0100

    fix conv_precision

--- model.py
+++ model.py
@@ -490,7 +490,7 @@ class pc_conv_network(nn.Module):
 			else:
 				# P = self.Precision[i+1].detach()
 				f = 0.5*sum(sum(
-					torch.mm(PE, (P(PE.view(self.bs, chan, self.dim[i+1][0], self.dim[i+1][0]))).view(self.p['bs'],-1).t())
+					torch.mm(PE, (self.Precision[i+1](PE.view(self.bs, chan, self.dim[i+1][0], self.dim[i+1][0]))).view(self.p['bs'],-1).t())
 					))
 			# print(f)
 
