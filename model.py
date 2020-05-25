4c49e80b079b3841cfdd2e04161938a461021cd7
Stefan Brugger <stefpb@gmail.com>
2020-05-25 23:15:45 +0100

    fix conv_precision

--- model.py
+++ model.py
@@ -138,22 +138,27 @@ class sym_conv2D(nn.Module):
 			# add other elements of central and semi-central filters:
 			for k in range(len(self.weight_values)):
 				print(k)
-				print(row.size() == [])
+				print(row.size() == torch.size([])
 				print(i)
 
 				if k < len(self.weight_values) - len(self.weight_values[i]):
 					# get from rows above:
 					for j in range(1, middle):
-						# if row.size()
-						row = torch.cat([row,
-							self.weight_values[k][-(1+j),i].view(1,-1).expand(8*j,-1)]) # 
+						if row.size() ==  torch.size([]):
+							row = torch.stack([row,
+								self.weight_values[k][-(1+j),i].view(1,-1).expand(8*j,-1)]) # 
+						else:
+							row = torch.cat([row,
+								self.weight_values[k][-(1+j),i].view(1,-1).expand(8*j,-1)]) # 
 				else:
 					for j in range(1, middle):
-						print(j)
-						print(8*j)
-						print(-(1+j))
-						row = torch.cat([row,
-							self.weight_values[i][-(1+j),k].view(1,-1).expand(8*j,-1)]) # 
+						if row.size() ==  torch.size([]):
+							row = torch.stack([row,
+								self.weight_values[i][-(1+j),k].view(1,-1).expand(8*j,-1)]) # 
+			
+						else:
+							row = torch.cat([row,
+								self.weight_values[i][-(1+j),k].view(1,-1).expand(8*j,-1)]) # 
 			
 			# need zeros in: centres bit, start of other elements bit (to be replaced with stuff from prev lines)
 			matrix.append(row)
