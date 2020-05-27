71bbfb998033737c39c457e7eb4750209f90abf4
Stefan Brugger <stefpb@gmail.com>
2020-05-27 15:17:58 +0100

    fix conv_precision

--- model.py
+++ model.py
@@ -196,7 +196,7 @@ class sym_conv2D(nn.Module):
 
 			# LHS
 			if i > 0:
-				if (start_centre -1) - rhs.size(1) > 0:
+				if start_centre - rhs.size(1) > 0:
 					
 					if centre_block.size(1) == 1:
 						pre_cov[start_centre-rhs.size(1):start_centre, start_centre] = lhs
