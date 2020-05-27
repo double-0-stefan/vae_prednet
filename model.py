b1eaeb3f599a7d5dd81a6797d4f3cd6a714f4a22
Stefan Brugger <stefpb@gmail.com>
2020-05-27 14:32:47 +0100

    fix conv_precision

--- model.py
+++ model.py
@@ -176,7 +176,7 @@ class sym_conv2D(nn.Module):
 			#RHS
 			if i < int(length/centre_block.size(0)) -1:
 				if end_centre +rhs.size(1)  < length:
-					pre_cov[end_centre+1 : end_centre +1+rhs.size(1), start_centre:end_centre] = rhs.t()
+					pre_cov[end_centre+1 : end_centre +1+rhs.size(1), start_centre:end_centre] = rhs
 				else:
 					pre_cov[end_centre+1 : end_centre+end_rhs, start_centre:end_centre] = rhs[:-end_centre]
 
