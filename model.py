1c529406d9cb44eb63c40dc1e9a14850cccd979a
Stefan Brugger <stefpb@gmail.com>
2020-05-28 12:46:47 +0100

    fix conv_precision

--- model.py
+++ model.py
@@ -192,7 +192,8 @@ class sym_conv2D(nn.Module):
 					if centre_block.size(1) == 1:
 						print(length)
 						print(end_centre)
-						pre_cov[end_centre:, start_centre] = rhs[:,:-(length-end_centre)].unsqueeze(0)
+						print(rhs[:,:-(length-end_centre)].size())
+						pre_cov[end_centre:, start_centre] = rhs[:,:-(length-end_centre)]
 					else:
 						pre_cov[end_centre:, start_centre:end_centre] = rhs[:,:-(length-end_centre)]
 
