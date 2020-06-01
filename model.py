9f3aa55a6fd673504401ba70c3e2b92bb32812bd
Stefan Brugger <stefpb@gmail.com>
2020-06-01 12:42:12 +0100

    toep

--- model.py
+++ model.py
@@ -519,7 +519,7 @@ class pc_conv_network(nn.Module):
 		for i in range(-1, len(self.Precision)):
 		
 			toep = self.Precision[i+1].convmatrix2d(self.Precision[i+1].filter_weights, 
-				self.phi[i].view(self.bs, self.chan[i+1][-1], self.dim[i+1][-1], self.dim[i+1][-1]
+				self.phi[i+1].view(self.bs, self.chan[i+1][-1], self.dim[i+1][-1], self.dim[i+1][-1]
 				))
 			print(toep)
 			print(toep.size())
