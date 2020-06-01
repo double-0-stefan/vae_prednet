185b0f5a7a22f9a29819b6416c34fab8d05a1a4e
Stefan Brugger <stefpb@gmail.com>
2020-06-01 20:21:42 +0100

    toep

--- model.py
+++ model.py
@@ -260,20 +260,20 @@ class sym_conv2D(nn.Module):
 						pre_cov[:start_centre, start_centre:end_centre] = lhs[:,-start_centre:].t()
 
 
-		# s = centre_block.size(1) +rhs.size(1) -1
+		s = centre_block.size(1) +rhs.size(1) -1
 
-		# A = pre_cov[:s, :s].to('cuda')
-		# B = pre_cov[:s, s:s+s].to('cuda')# upper triangle
-		# C = pre_cov[s:s+s, :s].to('cuda')# lower triangle
+		A = pre_cov[:s, :s].to('cuda')
+		B = pre_cov[:s, s:s+s].to('cuda')# upper triangle
+		C = pre_cov[s:s+s, :s].to('cuda')# lower triangle
 
-		# print(pre_cov.size())
-		# print(pre_cov[:,:])
-		# print(pre_cov[3,:])
+		print(pre_cov.size())
+		print(pre_cov[:,:])
+		print(pre_cov[3,:])
 
-		# self.register_buffer('A', A)
-		# self.register_buffer('B', B)
-		# self.register_buffer('C', C)
-		self.register_buffer('precov', precov)
+		self.register_buffer('A', A)
+		self.register_buffer('B', B)
+		self.register_buffer('C', C)
+		# self.register_buffer('pre_cov', pre_cov)
 
 
 
